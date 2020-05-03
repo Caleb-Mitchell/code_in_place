b3c5c3645b603e6910b32916dd77e6f4a288fa95
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-03 16:27:07 -0500

    figured out adding function to running total, cleaned up a bit

--- Section3/running_total_w_function.py
+++ Section3/running_total_w_function.py
@@ -8,12 +8,19 @@ dog age using the fact that there are seven dog years per human year.
 
 def main():
     num = int(input("Enter a value: "))
-    total = 0
+    print("Running total is", num)
+    print("")
+    total = num
     while num != 0:
-        total += num
+        total = running_total(total)
         print("Running total is", total)
         print("")
-        num = int(input("Enter a value: "))
+
+
+def running_total(total):
+    num = int(input("Enter a value: "))
+    total += num
+    return total
 
 
 if __name__ == '__main__':
