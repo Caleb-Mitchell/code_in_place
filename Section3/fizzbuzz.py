1005a1433775f85e8ca9a1fffb5732a4d4a4d1b3
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-05 15:33:10 -0500

    fizzbuzz works!

--- Section3/fizzbuzz.py
+++ Section3/fizzbuzz.py
@@ -1,9 +1,21 @@
 
 
+def main():
+    user_input = int(input("Number to count to: "))
+    fizzbuzz(user_input)
 
 
-
-
+def fizzbuzz(given_number):
+    for i in range(1, given_number + 1):
+        # if i % 15 == 0:
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("Fizzbuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
 
 
 if __name__ == '__main__':
