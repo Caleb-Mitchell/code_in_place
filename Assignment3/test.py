a777768be47ed8675f9e3dd9ad380d4d166e9ac0
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-05 13:52:58 -0500

    added T430 and fixed test program

---
+++ Assignment3/test.py
@@ -0,0 +1,11 @@
+
+from simpleimage import SimpleImage
+
+def main():
+    first_pic = SimpleImage('images/T430.png')
+    first_pic.show()
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == '__main__':
+    main()
