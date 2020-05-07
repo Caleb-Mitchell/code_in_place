5100142fdb4cf4239a41b6e7561507d975205eb9
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-07 13:44:43 -0500

    simplified trim_crop after section

--- Section4/trim_crop.py
+++ Section4/trim_crop.py
@@ -25,12 +25,10 @@ def trim_crop_image(filename, trim_size):
 
     cropped_image = SimpleImage.blank(width - (trim_size * 2), height - (trim_size * 2))
 
-    for y in range(height):
-        if trim_size < y < (height - trim_size):
-            for x in range(width):
-                if trim_size < x < (width - trim_size):
-                    pixel = image.get_pixel(x, y)
-                    cropped_image.set_pixel((x - trim_size), (y - trim_size), pixel)
+    for y in range(trim_size, (height - trim_size)):
+        for x in range(trim_size, (width - trim_size)):
+            pixel = image.get_pixel(x, y)
+            cropped_image.set_pixel((x - trim_size), (y - trim_size), pixel)
     return cropped_image
 
 
