e27b93a41d3f26e25619e1755a62949bb16e3b21
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-07 14:24:34 -0500

    finished add_border after section

--- Section4/add_border.py
+++ Section4/add_border.py
@@ -1,6 +1,6 @@
 from simpleimage import SimpleImage
 
-BRIGHTNESS_THRESHOLD = 153
+BORDER_SIZE = 10
 
 DEFAULT_FILE = 'images/simba-sq.jpg'
 
@@ -10,26 +10,41 @@ def main():
     filename = get_file()
     image = SimpleImage(filename)
 
-    # Show the original simba
-    original_simba = SimpleImage(filename)
-    original_simba.show()
+    # Show the original image
+    original_image = SimpleImage(filename)
+    original_image.show()
 
-    # Show simba with the narok filter
-    narok_simba = apply_narok(filename)
-    narok_simba.show()
+    # Show image with a black border
+    bordered_image = add_border(filename, BORDER_SIZE)
+    bordered_image.show()
 
 
-def apply_narok(filename):
-    image = SimpleImage(filename)
-    for pixel in image:
-        average = (pixel.red + pixel.green + pixel.blue) // 3
-        # see if this pixel is "bright"
-        if average > BRIGHTNESS_THRESHOLD:
-            # if so, make pixel greyscale
-            pixel.red = average
-            pixel.green = average
-            pixel.blue = average
-    return image
+def add_border(original_img, border_size):
+    """
+    This function returns a new SimpleImage which is the same as
+    original image except with a black border added around it. The
+    border should be border_size many pixels thick.
+
+    Inputs:
+        - original_img: The original image to process
+        - border_size: The thickness of the border to add around the image
+
+    Returns:
+        A new SimpleImage with the border added around original image
+    """
+    original_img = SimpleImage(original_img)
+    bordered_image = SimpleImage.blank((original_img.width + (border_size * 2)),
+                                       (original_img.height + (border_size * 2)), 'black')
+
+    # could also include a for each loop to create totally black background *first*,
+    # or, could try to implement a second double 4 loop to add black only in the border_with sized edges
+    # or, just use the .blank method parameter of a back_color, implemented here.
+
+    for y in range(original_img.height):
+        for x in range(original_img.width):
+            pixel = original_img.get_pixel(x, y)
+            bordered_image.set_pixel(x + border_size, y + border_size, pixel)
+    return bordered_image
 
 
 def get_file():
