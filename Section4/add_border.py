05f0c3f425d706f4510e0c1d1673e895b7ad80d5
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-06 23:32:58 -0500

    working on trim crop

---
+++ Section4/add_border.py
@@ -0,0 +1,44 @@
+from simpleimage import SimpleImage
+
+BRIGHTNESS_THRESHOLD = 153
+
+DEFAULT_FILE = 'images/simba-sq.jpg'
+
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original simba
+    original_simba = SimpleImage(filename)
+    original_simba.show()
+
+    # Show simba with the narok filter
+    narok_simba = apply_narok(filename)
+    narok_simba.show()
+
+
+def apply_narok(filename):
+    image = SimpleImage(filename)
+    for pixel in image:
+        average = (pixel.red + pixel.green + pixel.blue) // 3
+        # see if this pixel is "bright"
+        if average > BRIGHTNESS_THRESHOLD:
+            # if so, make pixel greyscale
+            pixel.red = average
+            pixel.green = average
+            pixel.blue = average
+    return image
+
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
---
+++ Section4/trim_crop.py
@@ -0,0 +1,44 @@
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/karel.png'
+TRIM_SIZE = 30
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original karel
+    original_karel = SimpleImage(filename)
+    original_karel.show()
+
+    # Show karel with the image cropped
+    trim_size = TRIM_SIZE
+    cropped_karel = trim_crop_image(filename, trim_size)
+    cropped_karel.show()
+
+
+def trim_crop_image(filename, trim_size):
+    image = SimpleImage(filename)
+    width = image.width
+    height = image.height
+
+    cropped_image = SimpleImage.blank(width - (trim_size * 2), height - (trim_size * 2))
+
+    for y in range((0 + trim_size), (height - trim_size)):
+        for x in range((0 + trim_size), (width - trim_size)):
+            pixel = image.get_pixel(x, y)
+            cropped_image.set_pixel(x, y, pixel)
+    return cropped_image
+
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
