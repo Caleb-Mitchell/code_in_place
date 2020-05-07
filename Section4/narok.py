f3d14bb330da8c8ff57a699759d6bdb670677f08
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-06 22:40:20 -0500

    narok works

--- Section4/narok.py
+++ Section4/narok.py
@@ -1,20 +1,47 @@
+
+from simpleimage import SimpleImage
+
+
 BRIGHTNESS_THRESHOLD = 153
 
-import simpleimage from SimpleImage
+
+DEFAULT_FILE = 'images/simba-sq.jpg'
+
 
 def main():
-	for pixel in image:
-			average = (pixel.red + pixel.green + pixel.blue) // 3
-			# see if this pixel is "sufficiently" green
-			if pixel.green >= average * INTENSITY_THRESHOLD:
-					# if so, overwrite pixel in original image with corresponding
-					# pixel from the back image.
-					x = pixel.x
-					y = pixel.y
-					image.set_pixel(x, y, back.get_pixel(x, y))
-			return image
+	# Get file and load image
+	filename = get_file()
+	image = SimpleImage(filename)
 
+	# Show the original simba
+	original_simba = SimpleImage(filename)
+	original_simba.show()
+
+	# Show simba with the narok filter
+	narok_simba = apply_narok(filename)
+	narok_simba.show()
+
+
+def apply_narok(filename):
+	image = SimpleImage(filename)
+	for pixel in image:
+		average = (pixel.red + pixel.green + pixel.blue) // 3
+		# see if this pixel is "bright"
+		if average > BRIGHTNESS_THRESHOLD:
+				# if so, make pixel greyscale
+			pixel.red = average
+			pixel.green = average
+			pixel.blue = average
+	return image
+
+
+def get_file():
+	# Read image file path from user, or use the default file
+	filename = input('Enter image file (or press enter): ')
+	if filename == '':
+		filename = DEFAULT_FILE
+	return filename
 
 
 if __name__ == '__main__':
-    main()
+	main()
