9730ea1981ebccba03711ba633ac65fc18cff06a
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-08 18:33:42 -0500

    warhol almost kinda works

--- Assignment3/warhol_filter.py
+++ Assignment3/warhol_filter.py
@@ -11,11 +11,33 @@ WIDTH = N_COLS * PATCH_SIZE
 HEIGHT = N_ROWS * PATCH_SIZE
 PATCH_NAME = 'images/simba-sq.jpg'
 
+
 def main():
     final_image = SimpleImage.blank(WIDTH, HEIGHT)
-    # TODO: your code here.
-    # This is an example which should generate a pinkish patch
-    patch = make_recolored_patch(1.5, 0, 1.5)
+
+    # assign simba picture to name 'simba_pic'
+    simba_pic = SimpleImage(PATCH_NAME)
+    # simba_pic = make_recolored_patch(1.5, 0, 1.5)
+
+    for x_patches in range(N_COLS):
+        for y_patches in range(N_ROWS):
+            # # pull all pixels from 'simba_pic', set to 'final image'
+            # in original location
+            for y in range(PATCH_SIZE):
+                for x in range(PATCH_SIZE):
+                    pixel = simba_pic.get_pixel(x, y)
+                    # this works for single image
+                    # final_image.set_pixel(x, y, pixel)
+                    # looking for multiple columns here
+                    final_image.set_pixel((x + (PATCH_SIZE * x_patches)), (y + (PATCH_SIZE * y_patches)), pixel)
+            simba_pic = make_recolored_patch(x_patches, y_patches, N_COLS)
+        # makes pink
+        # make_recolored_patch(1.5, 0, 1.5)
+
+    # # This is an example which should generate a pinkish patch
+    # patch = make_recolored_patch(1.5, 0, 1.5)
+
+    simba_pic.show()
     final_image.show()
 
 def make_recolored_patch(red_scale, green_scale, blue_scale):
@@ -28,8 +50,15 @@ def make_recolored_patch(red_scale, green_scale, blue_scale):
     :return: the newly generated patch
     '''
     patch = SimpleImage(PATCH_NAME)
-    # TODO: your code here.
+    for pixel in patch:
+        pixel.red *= red_scale
+        pixel.green *= green_scale
+        pixel.blue *= blue_scale
+        # pixel.red == red_scale
+        # pixel.green == green_scale
+        # pixel.blue == blue_scale
     return patch
 
 if __name__ == '__main__':
     main()
+
