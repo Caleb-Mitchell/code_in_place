194c789b8ef0278646f26eb9b30dd7fd37c3be51
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-05-08 20:03:30 -0500

    warhol works!

--- Assignment3/warhol_filter.py
+++ Assignment3/warhol_filter.py
@@ -14,30 +14,35 @@ PATCH_NAME = 'images/simba-sq.jpg'
 
 def main():
     final_image = SimpleImage.blank(WIDTH, HEIGHT)
-
+    color = 0
     # assign simba picture to name 'simba_pic'
     simba_pic = SimpleImage(PATCH_NAME)
-    # simba_pic = make_recolored_patch(1.5, 0, 1.5)
 
     for x_patches in range(N_COLS):
         for y_patches in range(N_ROWS):
-            # # pull all pixels from 'simba_pic', set to 'final image'
-            # in original location
+            if color == 0:
+                simba_pic = make_recolored_patch(1.5, 0, 1.5)
+                color += 1
+            elif color == 1:
+                simba_pic = make_recolored_patch(2, 2, 2)
+                color += 1
+            elif color == 2:
+                simba_pic = make_recolored_patch(0, 1.5, 1.5)
+                color += 1
+            elif color == 3:
+                simba_pic = make_recolored_patch(0, 0, 1.5)
+                color += 1
+            elif color == 4:
+                simba_pic = make_recolored_patch(1.5, 1.5, 0)
+                color += 1
+            elif color == 5:
+                simba_pic = make_recolored_patch(.5, .5, .5)
+                color += 1
             for y in range(PATCH_SIZE):
                 for x in range(PATCH_SIZE):
                     pixel = simba_pic.get_pixel(x, y)
-                    # this works for single image
-                    # final_image.set_pixel(x, y, pixel)
-                    # looking for multiple columns here
                     final_image.set_pixel((x + (PATCH_SIZE * x_patches)), (y + (PATCH_SIZE * y_patches)), pixel)
-            simba_pic = make_recolored_patch(x_patches, y_patches, N_COLS)
-        # makes pink
-        # make_recolored_patch(1.5, 0, 1.5)
 
-    # # This is an example which should generate a pinkish patch
-    # patch = make_recolored_patch(1.5, 0, 1.5)
-
-    simba_pic.show()
     final_image.show()
 
 def make_recolored_patch(red_scale, green_scale, blue_scale):
@@ -54,11 +59,7 @@ def make_recolored_patch(red_scale, green_scale, blue_scale):
         pixel.red *= red_scale
         pixel.green *= green_scale
         pixel.blue *= blue_scale
-        # pixel.red == red_scale
-        # pixel.green == green_scale
-        # pixel.blue == blue_scale
     return patch
 
 if __name__ == '__main__':
     main()
-
