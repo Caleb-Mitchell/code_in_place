b7f1bf059afd8ea0dca6e41e93a479e0bf500550
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-06-30 13:43:15 -0500

    progress on credit_card_total.py

--- discord_extension/Assignment5/credit_card_total.py
+++ discord_extension/Assignment5/credit_card_total.py
@@ -21,10 +21,27 @@ def main():
         for line in file:
             # strip and split
             line = line.strip()
-            line = line.split()     # FIXME split correctly, or join together certain indexes?
-            # add correct indices to dictionary
+            line = line.split()
+            # if index[1] does NOT end with ']', add index[2] to end of index[1]?
+            if line[1][-1] != ']':
+                new_str = ''.join(line[1]) + ' ' + str(line[2])
+                line[1] = new_str
+                line.pop(2)
 
+            # remove punctuation        TODO: should later use a loop like in the CIP notes
+            line[1] = line[1].replace('[', '')
+            line[1] = line[1].replace(']', '')
+            line[2] = line[2].replace('$', '')
+
+            # add correct indices to dictionary
             print(line)
+            if line[1] not in records:
+                records[line[1]] = int(line[2])
+            if line[1] in records:
+                records[line[1]] += int(line[2])
+
+
+    print(records)
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
