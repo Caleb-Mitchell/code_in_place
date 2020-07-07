a46ab3f1b0bccb248453d7c8a1104d080abb30ab
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2020-07-07 12:02:36 -0500

    part 1 of babynames done

--- discord_extension/Assignment6/data_analysis.py
+++ discord_extension/Assignment6/data_analysis.py
@@ -18,10 +18,38 @@ def load_data(filename):
     >>> load_data('disease1.txt')
     {'Evermore': [1, 1, 1, 1, 1, 1, 1], 'Vanguard City': [1, 2, 3, 4, 5, 6, 7], 'Excelsior': [1, 1, 2, 3, 5, 8, 13]}
     """
-    pass
-    """
-    You fill this in.  Don't forget to remove the 'pass' statement above.
-    """
+    # create empty dict to return later
+    data_dict = {}
+    # read through data file
+    with open(filename) as file:
+        for line in file:
+            # Test for name of city in line,
+            # Make new str of name
+            city_str = ''
+            for char in line:
+                if char.isalpha() or char == ' ':
+                    city_str += char
+
+            # strip leading/trailing spaces
+            city_str = city_str.strip()
+
+            # Make list out of line
+            line = line.strip()
+            line = line.split(',')
+
+            # Remove city name from list
+            line.pop(0)
+
+            # Remove extra spaces in list elements
+            line = [x.strip(' ') for x in line]
+
+            # Change list numbers to ints
+            for i in range(len(line)):
+                line[i] = int(line[i])
+
+            # add that str/list to dictionary
+            data_dict[city_str] = line
+        print(data_dict)
 
 
 def daily_cases(cumulative):
